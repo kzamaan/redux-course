55dfe6a83a392ee7143297a58dd0719a3ed30a18
Kamruzzaman <kzamanbn@gmail.com>
2023-02-26 10:08:11 +0600

    added template and redux

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = 'todo/added';
+export const TOGGLED = 'todo/toggled';
+export const COLOR_SELECTED = 'todo/toggled';
+export const DELETED = 'todo/delete';
+export const ALL_COMPLETED = 'todo/completed';
+export const CLEAR_COMPLETED = 'todo/clearCompleted';
