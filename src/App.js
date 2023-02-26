a02925d8ed701623b704fae543feba259c923cc9
Kamruzzaman <kzamanbn@gmail.com>
2023-02-26 13:59:35 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,11 @@ function App() {
 		<Provider store={store}>
 			<div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
 				<Navbar />
-
 				<div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
 					<Header />
 					<hr className="mt-4" />
-
 					<TodoList />
-
 					<hr className="mt-4" />
-
 					<Footer />
 				</div>
 			</div>
