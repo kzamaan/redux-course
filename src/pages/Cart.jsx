6f2d7bc820de2ab86a77173ac7ae020b6d90a27d
Kamruzzaman <kzamanbn@gmail.com>
2023-03-01 15:31:40 +0600

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -30,41 +30,43 @@ export default function Cart() {
 					)}
 				</div>
 
-				<div>
-					<div className="billDetailsCard">
-						<h4 className="mt-2 mb-8 text-xl font-bold text-center">Bill Details</h4>
-						<div className="space-y-4">
-							<div className="flex items-center justify-between">
-								<p>Sub Total</p>
-								<p>
-									BDT <span className="lws-subtotal">{subTotal}</span>
-								</p>
-							</div>
+				{cartWithProducts.length > 0 && (
+					<div>
+						<div className="billDetailsCard">
+							<h4 className="mt-2 mb-8 text-xl font-bold text-center">Bill Details</h4>
+							<div className="space-y-4">
+								<div className="flex items-center justify-between">
+									<p>Sub Total</p>
+									<p>
+										BDT <span className="lws-subtotal">{subTotal}</span>
+									</p>
+								</div>
 
-							<div className="flex items-center justify-between">
-								<p>Discount</p>
-								<p>
-									BDT <span className="lws-discount">0</span>
-								</p>
-							</div>
+								<div className="flex items-center justify-between">
+									<p>Discount</p>
+									<p>
+										BDT <span className="lws-discount">0</span>
+									</p>
+								</div>
 
-							<div className="flex items-center justify-between">
-								<p>VAT</p>
-								<p>
-									BDT <span className="vat">0</span>
-								</p>
-							</div>
+								<div className="flex items-center justify-between">
+									<p>VAT</p>
+									<p>
+										BDT <span className="vat">0</span>
+									</p>
+								</div>
 
-							<div className="flex items-center justify-between pb-4">
-								<p className="font-bold">TOTAL</p>
-								<p className="font-bold">
-									BDT <span className="lws-total">{subTotal}</span>
-								</p>
+								<div className="flex items-center justify-between pb-4">
+									<p className="font-bold">TOTAL</p>
+									<p className="font-bold">
+										BDT <span className="lws-total">{subTotal}</span>
+									</p>
+								</div>
+								<button className="placeOrderbtn">place order</button>
 							</div>
-							<button className="placeOrderbtn">place order</button>
 						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</div>
 	);
