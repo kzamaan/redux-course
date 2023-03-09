302b177e2001323785e2b39c44bfce870cab2ba3
Kamruzzaman <kzamanbn@gmail.com>
2023-03-09 19:53:42 +0600

    update fetch related video after fetching single video object

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { fetchVideos } = require('./features/videos/videoSlice');
 const { fetchRelatedVideos } = require('./features/videos/relatedVideoSlice');
 
 // dispatch the thunk function to fetch the videos and related videos
-store.dispatch(fetchVideos()).then(() => {
-	const state = store.getState();
-	const tags = state.video?.video?.tags;
-	if (tags) store.dispatch(fetchRelatedVideos(tags));
+store.dispatch(fetchVideos()).then((response) => {
+	if (response.meta.requestStatus === 'fulfilled') {
+		store.dispatch(fetchRelatedVideos(response.payload.tags));
+	}
 });
